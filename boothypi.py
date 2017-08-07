64ebe97f6a4add52885d04e5897de1df7fff0d20
Dick Visser <dickvisser1994@gmail.com>
2017-08-07 20:37:21 +0000

    Now creates required dirs.

--- boothypi.py
+++ boothypi.py
@@ -1,11 +1,36 @@
 from subprocess import call
 from time import sleep, gmtime, strftime
+import os
+import errno
 import RPi.GPIO as GPIO
 
-# Destination path of the preview file.
-PREVIEW_FILE_PATH = '/home/dick/dev/pi/boothypiui/feed/preview/preview.jpg'
+# Destination dir of the preview file.
+PREVIEW_FILE_DIR = '/home/pi/dev/boothypi-ui/feed/preview/'
+# Filename of the preview file.
+PREVIEW_FILE_NAME = 'preview.jpg'
+# Full path of the preview file.
+PREVIEW_FILE_PATH = PREVIEW_FILE_DIR + '' + PREVIEW_FILE_NAME
 # Destination directory for the camera roll.
-ROLL_DIR = '/home/dick/dev/pi/boothypiui/feed/roll/'
+ROLL_DIR = '/home/pi/dev/boothypi-ui/feed/roll/'
+
+def try_create_dir(path):
+  print('Creating ' + path)
+  try:
+    os.makedirs(path)
+    print('Successfully created ' + path)
+  except OSError as e:
+    if e.errno == errno.EEXIST:
+      print('Did not create dir because it already exists (' + path + ').')
+
+print('Creating required dirs')
+try_create_dir(PREVIEW_FILE_DIR)
+try_create_dir(ROLL_DIR)
+print('Required dirs created')
+
+PIN_SNAP_BUTTON = 23
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIN_SNAP_BUTTON, GPIO.IN)
 
 DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
 
@@ -31,7 +56,7 @@ def take_picture(channel):
   camera_busy = False
   # call(['cp', ]) # TODO: copy to external drive
 
-GPIO.add_event_detect(channel, GPIO.RISING, callback=take_picture)
+GPIO.add_event_detect(PIN_SNAP_BUTTON, GPIO.RISING, callback=take_picture)
 
 while True:
   if camera_busy == False:
