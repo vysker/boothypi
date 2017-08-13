e2c63de42334725834b289533c6b611d634aa2cc
Dick <dickvisser1994@gmail.com>
2017-08-13 20:50:37 +0200

    Made the application multithreaded.
    This enables us to reload the preview image without blocking the GTK main loop.

--- compile.sh
+++ compile.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Requires: sudo apt-get install libgtk-3-dev
+# Then reboot.
+
 src='src'
 dist='dist'
 # gcc "$src/base.c" -o "$dist/base" `pkg-config --cflags --libs gtk+-3.0`
--- src/base.c
+++ src/base.c
@@ -1,7 +1,9 @@
 #include <gtk/gtk.h>
 
-// Requires: sudo apt-get install libgtk-3-dev
-// Then reboot
+const gchar preview_image_path[] = "/home/dick/dev/pi/boothypiui-gtk/preview.jpg";
+int preview_image_width = 600;
+int preview_image_height = 400;
+GtkWidget *image;
 
 static void hello(GtkWidget *widget, gpointer data)
 {
@@ -13,13 +15,50 @@ static void destroy(GtkWidget *widget, gpointer data)
   gtk_main_quit();
 }
 
+static gpointer stream_preview_image(gpointer image)
+{
+  int num_errors = 0;
+  int num_images_loaded = 0;
+  int i = 0;
+
+  while (TRUE) {
+    GdkPixbuf *pixbuf;
+    GdkPixbuf *pixbuf2;
+    GError *error = NULL;
+
+    sleep(0.25);
+
+    pixbuf = gdk_pixbuf_new_from_file(preview_image_path, &error);
+    // pixbuf = gdk_pixbuf_new_from_file_at_size(preview_image_path, preview_image_width, preview_image_height, &error);
+    // pixbuf = gdk_pixbuf_new_from_file_at_scale(preview_image_path, preview_image_width, preview_image_height, TRUE, &error); // TODO: Not necessary to check aspect ratio, since I already know the dimensions.
+    if (error == NULL) {
+      pixbuf2 = gdk_pixbuf_scale_simple(pixbuf, preview_image_width, preview_image_height, GDK_INTERP_NEAREST);
+      gtk_image_set_from_pixbuf(GTK_IMAGE(image), pixbuf2);
+      g_clear_object(&pixbuf2);
+      num_images_loaded++;
+    } else {
+      // g_printf("Pixbuf load error: %s\n", error->message);
+      num_errors++;
+    }
+
+    g_clear_error(&error);
+    g_clear_object(&pixbuf);
+
+    i++;
+
+    if (i % 16 == 0) { // Display info every 16 iterations
+      g_printf("i: %i | images: %i | errors: %i\n", i, num_images_loaded, num_errors);
+    }
+  }
+
+  return NULL;
+}
+
 int main(int argc, char *argv[])
 {
   GtkWidget *window;
   GtkWidget *button;
   GtkWidget *box1;
-  GtkWidget *image;
-  GdkPixbuf *pixbuf;
 
   gtk_init(&argc, &argv);
 
@@ -38,16 +77,14 @@ int main(int argc, char *argv[])
   gtk_box_pack_start(GTK_BOX(box1), button, TRUE, TRUE, 0);
   gtk_widget_show(button);
 
-  // pixbuf = gdk_pixbuf_new_from_file("/home/dick/dev/pi/boothypiui-gtk/preview.jpg", NULL);
-  // pixbuf = gdk_pixbuf_scale_simple(pixbuf, 600, 400, GDK_INTERP_BILINEAR);
-  pixbuf = gdk_pixbuf_new_from_file_at_scale("/home/dick/dev/pi/boothypiui-gtk/preview.jpg", 100, 300, TRUE, NULL);
-  image = gtk_image_new_from_pixbuf(pixbuf);
-  // image = gtk_image_new_from_file("/home/dick/dev/pi/boothypiui-gtk/preview.jpg");
+  image = gtk_image_new();
   gtk_box_pack_start(GTK_BOX(box1), image, TRUE, TRUE, 0);
   gtk_widget_show(image);
 
   gtk_widget_show(box1);
   gtk_widget_show(window);
+
+  g_thread_new("StreamPreviewImage", stream_preview_image, image);
 
   gtk_main();
 
